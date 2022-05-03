558a5e08b17f501810164df330b7013b376a3920
Huncho-J <77946521+Huncho-J@users.noreply.github.com>
2022-05-03 13:54:05 -0700

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,13 +3,13 @@ const ethers = require("ethers")
 const ETHToken = artifacts.require("./ETHToken")
 const ETHBridge = artifacts.require("./ETHBridge")
 
-const BSCToken = artifacts.require("./BSCToken")
-const BSCBridge = artifacts.require("./BSCBridge")
+const PolyToken = artifacts.require("./PolyToken")
+const PolyBridge = artifacts.require("./PolyBridge")
 
 module.exports = async function (deployer, network, addresses) {
 
-    const name = "Bridge Token"
-    const symbol = "TBT"
+    const name = "Test Core Token"
+    const symbol = "TCT"
     const supply = ethers.utils.parseUnits('5000000', 'ether') // 1,000,000 Tokens
 
     // Ideally we first deploy to rinkeby, then we deploy to binance testnet.
@@ -26,35 +26,35 @@ module.exports = async function (deployer, network, addresses) {
         await token.setBridge(bridge.address)
     }
 
-    if (network === 'bsc_testnet') {
-        await deployer.deploy(BSCToken, name, symbol)
-        const token = await BSCToken.deployed()
+    if (network === 'mumbai_testnet') {
+        await deployer.deploy(PolyToken, name, symbol)
+        const token = await PolyToken.deployed()
 
-        await deployer.deploy(BSCBridge, token.address)
-        const bridge = await BSCBridge.deployed()
-
-        await token.setBridge(bridge.address)
-    }
-
-    if (network === 'development') {
-        let token, bridge
-
-        await deployer.deploy(ETHToken, name, symbol)
-        token = await ETHToken.deployed()
-
-        await token.mint(addresses[0], supply)
-
-        await deployer.deploy(ETHBridge, token.address)
-        bridge = await ETHBridge.deployed()
-
-        await token.setBridge(bridge.address)
-
-        await deployer.deploy(BSCToken, name, symbol)
-        token = await BSCToken.deployed()
-
-        await deployer.deploy(BSCBridge, token.address)
-        bridge = await BSCBridge.deployed()
+        await deployer.deploy(PolyBridge, token.address)
+        const bridge = await PolyBridge.deployed()
 
         await token.setBridge(bridge.address)
     }
+
+    // if (network === 'development') {
+    //     let token, bridge
+
+    //     await deployer.deploy(ETHToken, name, symbol)
+    //     token = await ETHToken.deployed()
+
+    //     await token.mint(addresses[0], supply)
+
+    //     await deployer.deploy(ETHBridge, token.address)
+    //     bridge = await ETHBridge.deployed()
+
+    //     await token.setBridge(bridge.address)
+
+    //     await deployer.deploy(BSCToken, name, symbol)
+    //     token = await BSCToken.deployed()
+
+    //     await deployer.deploy(BSCBridge, token.address)
+    //     bridge = await BSCBridge.deployed()
+
+    //     await token.setBridge(bridge.address)
+    // }
 } 
